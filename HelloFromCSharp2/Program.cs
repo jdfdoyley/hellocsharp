13094353c58b7d3eaea785e0988516175b8e18a1
Jason D'Oyley <jdfd.dev@icloud.com>
2023-10-06 12:06:32 -0400

    Format code for readability

--- HelloFromCSharp2/Program.cs
+++ HelloFromCSharp2/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, Everyone!");
+
 Console.WriteLine("Please enter your name: ");
 var name = Console.ReadLine();
+
 Console.WriteLine(name);
