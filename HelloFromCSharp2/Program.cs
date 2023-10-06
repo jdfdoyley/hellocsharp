49e386dc2a49c6121d020ff80939da49a67222ba
Jason D'Oyley <jdfd.dev@icloud.com>
2023-10-06 12:02:38 -0400

    Create a simple hello program

---
+++ HelloFromCSharp2/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, Everyone!");
+Console.WriteLine("Please enter your name: ");
+var name = Console.ReadLine();
+Console.WriteLine(name);
